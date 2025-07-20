57856f81c0eca0841f16ac78c3b2b3a16813ec64
Ted Ciafardini - personal <esciafardini@gmail.com>
2025-07-20 02:41:43 -0400

    i do love you

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ document.getElementById("harvester").style.display = "none"
 document.getElementById('harvester').play();
 
 const welcome = document.getElementById('test');
-const welcText = "there is so much grief to look forward to.  and not 'looking forward to' with a positive connotation.  feeling lost? call the number: +1 (479) 334 3592.  it isn't artifical intelligence. dont ask me that.  'artifical intelligence' makes me want to die. ";
+const welcText = "there is so much grief to look forward to.  and not 'looking forward to' with a positive connotation.  feeling lost? call the number: +1 (479) 334 3592.  it isn't artifical intelligence. don't ask me that.  'artificial intelligence' makes me want to die. ";
 
 welcome.textContent = '';
 
